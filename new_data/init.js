7e484b32591911e3eb0bf413c73dfc3910bb1457
pruthvitraj <pruthvipatil0058@gmail.com>
2025-07-11 11:33:42 +0530

    Update init.js

--- new_data/init.js
+++ new_data/init.js
@@ -3,7 +3,10 @@ const data = require("./creation_of_data");
 
 // connection of monogodb;
 async function main() {
-    await mongoose.connect('mongodb+srv://pruthvipatil0058:patil0058@cluster0.r6iz0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    await mongoose.connect('mongodb+srv://pruthvipatil0058:patil0058@cluster0.r6iz0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 }
 main().then(() => { console.log("connected successful to DB") }).catch(err => { console.log(err); })
 
